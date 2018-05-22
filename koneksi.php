932700af80032416fdc26ebc62677e2fe86dc39b
Satriaa <39279153+Satriaa@users.noreply.github.com>
2018-05-22 12:14:38 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host='localhost';
+$username='root';
+$password='';
+$database='latihan_vertama';
+$koneksi= mysqli_connect($host,$username,$password,$database);
+ ?>
---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, intial-scale=1">
+    <!-- <link rel="stylesheet" href="css/bootstrap.css">-->
+    <!-- <link rel="stylesheet" href="assets/css/main.css" /> -->
+    <h1 align="center">LOGIN MEMBER</h1> <hr><hr>
+    <title>Login Masuk</title> <br>
+  </head>
+  <body>
+    <form class="" action="aksi_login.php" method="post">
+      <table align="center">
+      <tr>
+        <td>Username</td>
+        <td><input type="text" name="username" value="" placeholder="Username"> </td>
+      </tr>
+      <tr>
+        <td>Password</td>
+        <td><input type="password" name="password" value="" placeholder="Password"> </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>
+          <br>
+          <button type="submit" class="" name="button">LOGIN</button>
+        </td>
+      </tr>
+      <tr>
+      </tr>
+      <tr>
+        <td colspan="2"> Belum Punya Akun? <br>
+        <a href="register.php">Daftar <<</a> </td>
+      </tr>
+    </table>
+  </form>
+      <script src="js/jquery-3.3.1.min.js" charset="utf-8"></script>
+      <script src="js/bootstrap.js" charset="utf-8"></script>
+  </body>
+</html>
